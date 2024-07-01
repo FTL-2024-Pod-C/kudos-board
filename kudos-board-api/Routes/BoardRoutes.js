f80ee29381b3cd0cdcd18657d2a8ed891ae56e6b
Karina Perez <perezkarina376@gmail.com>
2024-07-01 16:09:33 -0700

    modified baord controller, route, and model to include adding and deleting cards from a board

--- kudos-board-api/Routes/BoardRoutes.js
+++ kudos-board-api/Routes/BoardRoutes.js
@@ -7,9 +7,11 @@ router.get("/", boardController.getAllBoards);
 router.get("/:id", boardController.getBoardById);
 
 router.post("/", boardController.createBoard);
+router.post("/:board_id/cards", boardController.addCardToBoard);
 
 router.put("/:id", boardController.updateBoard);
 
 router.delete("/:id", boardController.deleteBoard);
+router.delete("/:board_id/cards/:card_id", boardController.deleteCardFromBoard);
 
 module.exports = router;
