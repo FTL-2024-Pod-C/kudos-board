d3ff434fccd2c8b0e3d78cb24c3794efbd021a71
Karina Perez <perezkarina376@gmail.com>
2024-07-01 13:48:59 -0700

    added board schema, model, controller, route

---
+++ kudos-board-api/Models/BoardModel.js
@@ -0,0 +1,44 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// gets all the boards or gets the boards by a specific category
+const getAllBoards = async (filter = {}, orderBy = {}) => {
+    return prisma.board.findMany({
+        where: filter,
+        orderBy: orderBy,
+      });
+};
+
+
+const getBoardById = async (id) => {
+    return prisma.board.findUnique({
+        where: {id: parseInt(id)}
+    });
+};
+
+const createBoard = async (boardData) => {
+    return prisma.board.create({
+        data: boardData 
+    });
+};
+
+const updateBoard = async (id, boardData) => {
+    return prisma.board.update({
+        where: {id: parseInt(id)},
+        data: boardData
+    });
+};
+
+const deleteBoard = async (id) => {
+    return prisma.board.delete({
+        where: {id: parseInt(id)}
+    });
+};
+
+module.exports = {
+    getAllBoards,
+    getBoardById,
+    createBoard,
+    updateBoard,
+    deleteBoard
+};
--- kudos-board-api/server.js
+++ kudos-board-api/server.js
@@ -1,10 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require("cors");
+const boardRoutes = require("./Routes/BoardRoutes");
 const port = 3000;
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req,res) => {
     res.send("Hello World");
 });
+
+app.use("/", boardRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
