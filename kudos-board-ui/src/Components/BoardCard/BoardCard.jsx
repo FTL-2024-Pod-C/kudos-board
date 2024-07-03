e5bacdbac71fabddd96ab6c7c57d2990e38cbc67
madelds <sibalmadel@gmail.com>
2024-07-02 20:26:51 -0700

    added styling

--- kudos-board-ui/src/Components/BoardCard/BoardCard.jsx
+++ kudos-board-ui/src/Components/BoardCard/BoardCard.jsx
@@ -1,27 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import "./BoardCard.css"
 
 const BoardCard = (props) => {
 
-const deleteBoard = async () => {
-    try {
-        await axios.delete(`http://localhost:3000/boards/${boardId}`);
-        navigate("/");
-    } catch (error) {
-        console.error("Error deleting the board:", error);
-    }
-};
-
 return (
     <>
+   
         <div className="card">
             <img src={props.img} alt={props.title} />
             <h2 id="boardTitle">{props.title}</h2>
             <div className="boardBtn">
+            <Link to="/board">
                 <button>View Board</button>
-                <button>Delete Board</button>
+            </Link>
+            <button onClick={() => props.deleteBoard(props.board.id)}>Delete Board</button>
             </div>
         </div>
+   
     </>
     )
 }
