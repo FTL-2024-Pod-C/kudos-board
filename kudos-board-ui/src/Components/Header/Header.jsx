05bc2799040d65dbd9ca211f2f397ccc0954bdef
Julia Chaidez <jchaidez@jchaide-ltmc2wd.internal.salesforce.com>
2024-07-03 11:44:52 -0700

    added cutie lil frog icon

--- kudos-board-ui/src/Components/Header/Header.jsx
+++ kudos-board-ui/src/Components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import "./Header.css"
+import "./Header.css";
+import logo from "./logo.png"
 
 const Header = ({searchInputValue, handleOnSearchInputChange}) => {
     return (
     <>
         <div className="app-header">
+            <div className="head">
+            <img src={logo} alt="Logo" className="logo" /> {/* Use imported image */}
             <h1 className="child1">Kudos Board</h1>
+            </div>
             <input
               className="searchBar"
               type="text"
