01617c9b0ace12f18c5cde59f105b0fd81866e1c
Julia Chaidez <jchaidez@jchaide-ltmc2wd.internal.salesforce.com>
2024-07-02 19:09:37 -0700

    Cleaning up the App.jsx file by adding pages

--- kudos-board-ui/src/App.jsx
+++ kudos-board-ui/src/App.jsx
@@ -1,77 +1,20 @@
-import React from 'react'
-import { useState, useEffect } from "react";
-import "./App.css";
-import axios from "axios";
-import Header from "./Components/Header/Header.jsx";
-import BoardGrid from './Components/BoardGrid/BoardGrid.jsx';
-import Footer from "./Components/Footer/Footer.jsx";
-import NavBar from "./Components/NavBar/NavBar.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from "./Pages/HomePage.jsx"
+import BoardPage from './Pages/BoardPage.jsx';
+import NoPage from "./Pages/NoPage.jsx"
 
-const DEV_BASE_URL = "http://localhost:3000"
 
 function App() {
 
-  const [searchInputValue, setSearchInputValue] = useState("");
-  const [activeCategory, setActiveCategory] = useState("All");
-  const [boards, setBoards] = useState([]);
-
-  useEffect (() => {
-    const fetchBoards = async () => {
-    const url = `${DEV_BASE_URL}/boards`;
-    try {
-      const response = await axios.get(url);
-      console.log(response.data);
-      setBoards(response.data);
-    }
-    catch (error) {
-      console.error("Error fetching boards", error);
-    }
-    }
-    fetchBoards();
-  }, [])
-
-  const handleOnSearchInputChange = (event) => {
-    setSearchInputValue(event.target.value);
-  };
-
-  const boardsByCategory =
-    Boolean(activeCategory) && activeCategory !== "All"
-      ? boards.filter((p) => p.category === activeCategory)
-      : boards
-
-  const boardsToShow = Boolean(searchInputValue)
-    ? boardsByCategory.filter((p) => p.title.toLowerCase().indexOf(searchInputValue.toLowerCase()) !== -1)
-    : boardsByCategory
-
-  const createNewBoard = async (newBoard) => {
-    try {
-      const url = `${DEV_BASE_URL}/boards`;
-      const response = await axios.post(url, newBoard);
-      console.log(response.data);
-      setBoards([...boards, response.data]);
-    }
-    catch (error) {
-      console.error("Error creating a new board", error);
-    }
-  }
-
   return (
     <>
-      <div className="App">
-        <Header
-            searchInputValue={searchInputValue}
-            handleOnSearchInputChange={handleOnSearchInputChange}
-        />
-        <NavBar
-            createNewBoard={createNewBoard}
-            activeCategory={activeCategory}
-            setActiveCategory={setActiveCategory}
-          />
-        <BoardGrid 
-          boards={boardsToShow}
-        />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/board" element={<BoardPage />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
